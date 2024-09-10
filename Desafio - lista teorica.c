b2fd1060499c8b456f8750ce2cc67d7985ad4fa8
Feguimaraes02 <felipeguima0230@gmail.com>
2024-09-09 22:22:29 -0300

    Update Desafio - lista teorica.c

--- Desafio - lista teorica.c
+++ Desafio - lista teorica.c
@@ -21,7 +21,7 @@ u1 = vi1/24.13;
 u2 = vi2/11;
 u3 = vi3/18.65;
 
-printf("Voce vai comprar %d de unidades da acao 1, %d de unidades da acao 2, %d de unidades da acao 3.", u1, u2, u3);
+printf("Voce vai comprar %d unidades da acao 1, %d unidades da acao 2, %d unidades da acao 3.", u1, u2, u3);
 
 float dv1, dv2, dv3;
 
@@ -29,7 +29,7 @@ dv1 = (17/24.13)*100;
 dv2 = (35/11)*100;
 dv3 = (25/38.65)*100;
 
-printf("\nO dividend yield de que acao eh: \nAcao 1; %.2f,\nAcao2: %.2f,\nAcao3: %.2f", dv1, dv2, dv3);
+printf("\nO dividend yield de cada acao eh: \nAcao 1: %.2f,\nAcao 2: %.2f,\nAcao 3: %.2f. ", dv1, dv2, dv3);
 
 return 0;
 
